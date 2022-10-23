512f83abbab46c72fdc16d748b207482e5d95ccb
“mcurren” <mike@curren.me>
2022-10-22 21:19:06 -0500

    recent work

---
+++ src/lib/Text.svelte
@@ -0,0 +1,30 @@
+<script>
+	import prompts from '$lib/data/prompts.json';
+
+	function getWord( type ) {
+		const words = prompts[type];
+		const rand = Math.floor(Math.random() * words.length)
+		return words[rand];
+	}
+</script>
+
+<h1>"Find the <mark>{getWord('noun')}</mark> and stop the <mark>{getWord('villain')}</mark> from <mark>{getWord('action')}</mark> <mark>{getWord('target')}</mark> before <mark>{getWord('time')}</mark>!"</h1>
+
+<style>
+	h1 {
+		color: black;
+		font-size: clamp(1.5rem, 3vw + 1rem, 3.265rem);
+		text-align: center;
+		max-width: 70rem;
+		padding: 0 30px;
+		margin: 0;
+		margin-bottom: clamp(2rem, 2vw + 1rem, 4rem);
+		line-height: 1.5;
+		text-transform: lowercase;
+	}
+	mark {
+		background-color: rgb(255,231,25);
+		border: 5px solid rgb(230,244,252);
+		margin: 0;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,81 @@
+<script>
+	import Text from '$lib/Text.svelte';
+
+	const restart = () => {
+		location.reload();
+	}
+</script>
+
+<div>
+	<Text/>
+	<button class="flag-shape" on:click={restart}><span>hard pass</span></button>
+</div>
+
+<style>
+	@import url('https://fonts.googleapis.com/css2?family=New+Rocker&display=swap');
+	:global(html) {
+		box-sizing: border-box;
+		-moz-text-size-adjust: none;
+		-webkit-text-size-adjust: none;
+		text-size-adjust: none;
+	}
+	:global(*, *:before, *:after) {
+		box-sizing: inherit;
+		letter-spacing: inherit;
+	}
+	:global(body) {
+		margin: 0;
+		background: rgb(230,244,252) url('$lib/assets/grit.png') no-repeat;
+  	background-size: cover;
+		font-family: 'New Rocker', cursive;
+		font-size: 62.5%;
+		letter-spacing: 0.0875rem;
+	}
+	div {
+		width: 100%;
+		height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+		flex-direction: column;
+	}
+	button {
+		background: black;
+		border: 0;
+		border-radius: 0;
+		color: white;
+		font-family: inherit;
+		text-transform: lowercase;
+		font-size: 1.3rem;
+		padding: 0.3rem 0.5rem;
+		transition: all 0.3s;
+		cursor: pointer;
+	}
+	button:hover,
+	button:focus,
+	button:active {
+		background: red;
+	}
+	button:active {
+		transform: translateY(5px);
+	}
+	.flag-shape {
+		width: 130px;
+		height: 56px;
+		box-sizing: content-box;
+		padding: 7px 5px 15px;
+		position: relative;
+		text-align: center;
+	}
+	.flag-shape:after {
+		content: "";
+		position: absolute;
+		left: 0;
+		bottom: 0;
+		width: 0;
+		height: 0;
+		border-bottom: 10px solid rgb(230,244,252);
+		border-left: 70px solid transparent;
+		border-right: 70px solid transparent;
+	}
+</style>
