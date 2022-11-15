320c4a6f6368092ec9fa8f8bc96e9b351f16f7a6
“mcurren” <mike@curren.me>
2022-11-15 16:14:16 -0600

    ability to change individual prompts on the page

--- src/lib/Text.svelte
+++ src/lib/Text.svelte
@@ -1,14 +1,26 @@
 <script>
-	import prompts from '$lib/data/prompts.json';
+import prompts from '$lib/data/prompts.json';
 
-	function getWord( type ) {
-		const words = prompts[type];
-		const rand = Math.floor(Math.random() * words.length)
-		return words[rand];
-	}
+function getPrompt( type ) {
+	const words = prompts[type];
+	const rand = Math.floor(Math.random() * words.length)
+	return words[rand];
+}
+
+function changePrompt( type ) {
+	hook[type] = getPrompt(type);
+}
+
+let hook = {
+	noun: getPrompt('noun'),
+	villain: getPrompt('villain'),
+	action: getPrompt('action'),
+	target: getPrompt('target'),
+	time: getPrompt('time'),
+};
 </script>
 
-<h1>Find the <mark data-type="noun">{getWord('noun')}</mark> and stop the <mark data-type="villain">{getWord('villain')}</mark> from <mark data-type="action">{getWord('action')}</mark> <mark data-type="target">{getWord('target')}</mark> before <mark data-type="time">{getWord('time')}</mark>!</h1>
+<h1>Find the <mark><button on:click={() => changePrompt('noun')}>{hook.noun}</button></mark> and stop the <mark><button on:click={() => changePrompt('villain')}>{hook.villain}</button></mark> from <mark><button on:click={() => changePrompt('action')}>{hook.action}</button></mark> <mark><button on:click={() => changePrompt('target')}>{hook.target}</button></mark> before <mark><button on:click={() => changePrompt('time')}>{hook.time}</button></mark>!</h1>
 
 <style>
 	h1 {
@@ -29,6 +41,7 @@
 		content: '\201D';
 	}
 	mark {
+		display: inline-block;
 		position: relative;
 		white-space: nowrap;
 		background: none transparent;
@@ -37,11 +50,20 @@
 		content: '';
 		position: absolute;
 		top: 0;
-		right: -0.03125em;
+		right: -0.0625em;
 		bottom: 0;
-		left: -0.03125em;
+		left: -0.0625em;
 		z-index: -1;
 		background: rgb(255,231,25);
 		background: linear-gradient(180deg, rgba(255,255,255,0) 37%, rgba(255,231,25,1) 37%, rgba(255,231,25,1) 76%, rgba(255,255,255,0) 76%);
+	}
+	button {
+		border: none;
+		padding: 0;
+		margin: 0;
+		font: inherit;
+		background: none transparent;
+		color: inherit;
+		cursor: pointer;
 	}
 </style>
